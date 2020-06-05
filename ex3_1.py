610ac251e060b30b9badbca54367b499dd583bb6
Todd Riemenschneider <twr14152@gmail.com>
2020-06-04 21:12:43 -0400

    update

--- ex3_1.py
+++ ex3_1.py
@@ -2,30 +2,39 @@ from bottle import post, get, delete, run
 
 @get('/')
 def index():
-    return "Welcome to bottle"
+    return "Get method"
 
 @post('/')
 def index_post():
-    return "Welcome from POST"
+    return "POST method"
 
 @delete('/')
 def index_delete():
-    return "Welcome from DELETE"
+    return "DELETE method"
 
 
 run(host = '0.0.0.0', port = 8080, debug = True)
 
 '''
->>> import requests
->>> requests.post('http://192.168.1.121:8080')
-<Response [200]>
->>> requests.post('http://192.168.1.121:8080').text
-'Welcome from POST'
->>> requests.get('http://192.168.1.121:8080').text
-'Welcome to bottle'
->>> requests.delete('http://192.168.1.121:8080').text
-'Welcome from DELETE'
->>> requests.delete('http://192.168.1.121:8080')
-<Response [200]>
->>>
+In [1]: import requests                                                                        
+
+In [2]: requests.get("http://192.168.1.121:8080")                                              
+Out[2]: <Response [200]>
+
+In [3]: requests.get("http://192.168.1.121:8080").text                                         
+Out[3]: 'Get method'
+
+In [4]: requests.post("http://192.168.1.121:8080")                                             
+Out[4]: <Response [200]>
+
+In [5]: requests.post("http://192.168.1.121:8080").text                                        
+Out[5]: 'POST method'
+
+In [6]: requests.delete("http://192.168.1.121:8080")                                           
+Out[6]: <Response [200]>
+
+In [7]: requests.delete("http://192.168.1.121:8080").text                                      
+Out[7]: 'DELETE method'
+
+In [8]:   
 '''
